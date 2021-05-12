499d18731fd24e602fc65012e366e6d7cbf84489
Az21 <39803478+Az-21@users.noreply.github.com>
2021-05-12 18:30:40 +0530

    ✔ added save and reset all buttons

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,6 +5,7 @@
 	import Selector from '../components/Selector.svelte';
 	import DisplayComponents from '../components/DisplayComponents.svelte';
 	import Footer from '../components/Footer.svelte';
+	import Button from '../components/Button.svelte';
 
 	// Page dimension constants
 	export let center: string = 'flex w-4/5 mx-auto';
@@ -15,7 +16,7 @@
 	let canvasHeight: number = 512;
 
 	// Mount canvas
-	let canvas: { getContext: (arg0: string) => any };
+	let canvas: HTMLCanvasElement;
 	onMount(() => {
 		const ctx = canvas.getContext('2d');
 		ctx.font = '20px Arial';
@@ -35,7 +36,7 @@
 	function createImage() {
 		const ctx = canvas.getContext('2d');
 		const drawImage = (url: string, yPosition: number) => {
-			const image = new Image();
+			let image = new Image();
 			image.src = url;
 			image.onload = () => {
 				ctx.drawImage(image, 0, yPosition);
@@ -50,6 +51,31 @@
 		drawImage(eyes[eyesId], yEyes);
 	}
 
+	function savePNG() {
+		alert(
+			"Please right click on image and select 'Save image as' to download your Hollow Knight avatar."
+		);
+
+		/*
+
+		// DISABLED: Image Bucket does not append CORS
+		// Hours of life gone. Reduced to dust.
+
+		var link = document.getElementById('link'); // add an empty anchor to make it work
+		link.setAttribute('download', 'hollow-avatar.png');
+		link.setAttribute(
+			'href',
+			canvas.toDataURL('image/png').replace('image/png', 'image/octet-stream')
+		);
+		link.click();
+		*/
+	}
+
+	// Reset everything by reloading page
+	function resetCanvas() {
+		location.reload();
+	}
+
 	// Display all available variants within a category
 	let category = 0;
 	function changeCategory(num: number) {
@@ -59,15 +85,23 @@
 
 <Navbar />
 
-<div class="{center} {spacing} flex md:flex-col lg:flex-row justify-between space-x-5">
+<div class="{center} {spacing} flex flex-col lg:flex-row justify-between space-x-5">
 	<!-- Canvas -->
-	<div class="flex justify-center">
+	<div class="flex flex-col justify-center">
 		<canvas
-			class="border-8 border-green-500 rounded-lg"
+			class="border-8 border-purple-500 rounded-lg"
 			bind:this={canvas}
 			width={canvasWidth}
 			height={canvasHeight}
 		/>
+		<div class="flex my-5 justify-around">
+			<div on:click={() => savePNG()}>
+				<Button text="Download Avatar" color="bg-purple-500" />
+			</div>
+			<div on:click={() => resetCanvas()}>
+				<Button text="Reset Everything" color="bg-purple-500" />
+			</div>
+		</div>
 	</div>
 
 	<!-- Avatar components -->
