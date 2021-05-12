39dfab7ddd29a0e851045d91b2722e5c37510d89
Az21 <39803478+Az-21@users.noreply.github.com>
2021-05-12 11:15:42 +0530

    🌠slider style

--- src/components/Selector.svelte
+++ src/components/Selector.svelte
@@ -8,34 +8,69 @@
 </script>
 
 <div class="bg-gray-900 rounded-xl p-6">
-	<div class="divide-y-2 divide-blue-600">
+	<div class="divide-y-2 divide-green-500">
 		<p class="text-lg font-medium">Customize {componentName}</p>
 		<p />
 	</div>
-	<div class="mt-2 grid grid-cols-2 gap-x-8 gap-y-4">
+	<div class="mt-2 grid grid-cols-2 gap-x-8 gap-y-4 items-end">
 		<!-- Row 1 -->
-		<p class="font-thin items-end">{componentName} ID</p>
+		<p class="font-thin text-lg">{componentName} ID</p>
 		<input
 			type="number"
-			class="bg-transparent ring-2 ring-blue-600 w-full flex text-center"
+			class="bg-transparent ring-2 ring-green-500 w-full h-8 flex text-center"
 			bind:value={componentId}
 			min="0"
 			max={componentCount}
 		/>
 
 		<!-- Row 2 -->
-		<p>Position</p>
+		<p class="font-thin text-lg">Position</p>
 		<div class="slidecontainer">
-			<input type="range" min="-240" max="240" bind:value={yPos} class="slider w-full" />
+			<input type="range" min="-240" max="240" bind:value={yPos} class="slider w-full h-8" />
 		</div>
 
 		<!-- Row 3 -->
 		<div on:click={() => (componentId = 0)}>
-			<Button text="Transparent" color="bg-blue-600" expand={true} />
+			<Button text="Transparent" color="bg-green-500" expand={true} />
 		</div>
-
 		<div on:click={() => (yPos = 0)}>
-			<Button text="Reset" color="bg-blue-600" expand={true} />
+			<Button text="Reset" color="bg-green-500" expand={true} />
 		</div>
 	</div>
 </div>
+
+<style>
+	.slidecontainer {
+		width: 100%;
+	}
+	.slider {
+		-webkit-appearance: none;
+		appearance: none;
+		width: 100%;
+		background: #d3d3d3;
+		outline: none;
+		opacity: 1;
+		-webkit-transition: 0.2s;
+		transition: opacity 0.2s;
+	}
+	.slider:hover {
+		opacity: 1;
+	}
+
+	.slider::-webkit-slider-thumb {
+		-webkit-appearance: none;
+		appearance: none;
+		width: 20px;
+		height: 40px;
+		background: #2563eb;
+		cursor: pointer;
+		border-radius: 2px;
+	}
+
+	.slider::-moz-range-thumb {
+		width: 25px;
+		height: 25px;
+		background: #2563eb;
+		cursor: pointer;
+	}
+</style>
